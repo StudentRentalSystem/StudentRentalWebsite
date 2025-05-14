53b703784def3d1b4d60b38a7eba57672fa36ae8
Jess <learningen13@gmail.com>
2025-05-14 09:08:12 +0800

    First commit

---
+++ src/main/java/xyz/jessyu/studentrentalwebsite/StudentRentalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package xyz.jessyu.studentrentalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRentalWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRentalWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/jessyu/studentrentalwebsite/StudentRentalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jessyu.studentrentalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRentalWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
