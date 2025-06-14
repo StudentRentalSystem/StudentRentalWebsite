3f1fa55d74cf704ad6f253155a4e78360048ab73
Hding49 <87240249+hding4915@users.noreply.github.com>
2025-06-14 16:43:09 +0800

    Use new mongo query prompt with querygenerator:1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     annotationProcessor("org.projectlombok:lombok")
-    implementation("io.github.studentrentalsystem:querygenerator:1.0.2")
+    implementation("io.github.studentrentalsystem:querygenerator:1.0.3")
     implementation("io.github.studentrentalsystem:llmdataparser:1.0.2")
 
 }
